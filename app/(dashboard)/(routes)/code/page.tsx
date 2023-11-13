badd6a0d093053a753d9d32b51535365aff04295
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-13 11:41:50 +0800

    little change (#5)
    * Initialized navigation area
    
    * complete the Sidebar component
    
    * finish the dashboard component
    
    * cannot get the interface of gpt-3.5
    
    * little change

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import OpenAI from "openai";
+import Heading from "@/sections/Heading";
+import { Code } from "lucide-react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  propt: z.string().min(2).max(50),
+});
+
+const CodePage = () => {
+  const test = async () => {
+    const openai = new OpenAI({
+      apiKey: "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",
+      dangerouslyAllowBrowser: true,
+    });
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(completion.choices[0]);
+  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      propt: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  const isLoading = form.formState.isSubmitting;
+  return (
+    <div>
+      <Heading title="Code Generation" description="Generate code using descriptive text." icon={Code} iconColor="text-green-700" bgColor="bg-green-700/10" />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                control={form.control}
+                name="propt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Simple toggle button using react hooks."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button onClick={test} disabled={isLoading} className="col-span-12 lg:col-span-2 w-full" type="submit">
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
